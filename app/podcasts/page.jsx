82a7bb7c900ee89a67c35c403a1d515062dd247a
mycallCampbell <mycallcampbell@gmail.com>
2023-12-13 22:32:36 +0000

    remove folder

--- app/podcasts/page.jsx
+++ app/podcasts/page.jsx
@@ -1,7 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import styles from './podcasts.module.css';
-import PodcastDetails from './[id]/PodcastDetails';
 
 async function getPodcasts() {
     
