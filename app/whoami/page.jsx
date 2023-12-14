99c9bbba7b9bd5a3a2649f40e6dfa3cb2c7c3430
mycallCampbell <mycallcampbell@gmail.com>
2023-12-14 01:20:25 +0000

    added content

--- app/whoami/page.jsx
+++ app/whoami/page.jsx
@@ -31,7 +31,13 @@ export default function Whoami() {
 
         <div className={styles.titleMain}>WHOAMI</div>
         <div className={styles.titleTag}>My Journey As a Computer Programmer: 7+ Years of Building Digital Worlds</div>
-      <div className={styles.contentMain}>hello</div>
+        <div className={styles.contentMain}>
+          Welcome Tech enthusiasts! I go by the alias Mycall Dev and 
+          Im excited to share my journey as a computer programmer with you today. 
+          With over 7 years in the field, Ive had the opportunity to delve into various 
+          facets of technology, gaining expertise in Networking, Cyber Security, Linux, 
+          and Full Stack Development.
+        </div>
         {/*<div className={styles.contentMain}>Welcome tech enthusiasts! I go by the alias Mycall Dev and 
           Im excited to share my journey as a computer programmer with you today. 
           With over 7 years in the field, Ive had the opportunity to delve into various 
